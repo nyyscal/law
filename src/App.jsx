e522b3aeac5428fc6e4f0bb11c0171399f90858b
KHATRIRAMESH <khatriramesh972@gmail.com>
2024-11-28 11:50:19 +0545

    routes config

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>this is home</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/service" element={<Services />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
