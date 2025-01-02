3871ddcdc1bcd932393a00d9608a9dc847f2e1b7
Nischal <nyyscal541556@gmail.com>
2025-01-02 12:04:17 +0545

    Minor Changes 2.0.5

--- src/components/Admin/NotificationDropdown.jsx
+++ src/components/Admin/NotificationDropdown.jsx
@@ -1,13 +1,13 @@
 const NotificationDropdown = ({ notifications }) => {
   return (
-    <div className="absolute left-1/2 -translate-x-2/3 ml-2 md:right-0 mt-2 w-64 bg-gray-300 shadow-lg rounded-lg z-50">
+    <div className="absolute left-1/2 -translate-x-2/3 ml-2 md:right-0 mt-2 w-64 bg-black text-[#FFD700] border border-[#FFD700] shadow-lg rounded-lg z-50">
       <h3 className="text-lg font-semibold p-2 border-b">Notifications</h3>
       <ul className="max-h-64 overflow-y-auto">
         {notifications.length > 0 ? (
           notifications.map((notification, index) => (
             <li
               key={index}
-              className="p-2 hover:bg-gray-600 border-b last:border-none text-sm"
+              className="p-2 hover:bg-gray-700 border-b last:border-none text-sm"
             >
               {notification}
             </li>
