bd54020243b6bc8afe84247e875c464f194d1416
KHATRIRAMESH <khatriramesh972@gmail.com>
2025-01-03 17:53:28 +0545

    dynamic notifications

--- src/components/Admin/NotificationDropdown.jsx
+++ src/components/Admin/NotificationDropdown.jsx
@@ -1,15 +1,21 @@
-const NotificationDropdown = ({ notifications }) => {
+import { useSelector } from "react-redux";
+
+const NotificationDropdown = () => {
+  const notifications = useSelector((state) => state.user.notifications);
+  console.log(notifications);
+  // console.log(typeof notifications);
+
   return (
     <div className="absolute left-1/2 -translate-x-2/3 ml-2 md:right-0 mt-2 w-64 bg-black text-[#FFD700] border border-[#FFD700] shadow-lg rounded-lg z-50">
       <h3 className="text-lg font-semibold p-2 border-b">Notifications</h3>
       <ul className="max-h-64 overflow-y-auto">
-        {notifications.length > 0 ? (
+        {notifications?.length > 0 ? (
           notifications.map((notification, index) => (
             <li
               key={index}
               className="p-2 hover:bg-gray-700 border-b last:border-none text-sm"
             >
-              {notification}
+              {notification.message}
             </li>
           ))
         ) : (
